a7b46dc409051327cbd1d1a111cf83adb6d9a910
Francisco Juan <juansoussev@gmail.com>
2023-10-21 19:42:13 -0300

    adicionando o path /projects

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App />} />
+        <Route path='/projects' />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
