dcee289db7c58f17a450b4eb0d4aea19d1fd82ba
Francisco Juan <juansoussev@gmail.com>
2023-10-21 20:35:37 -0300

    home content added

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,16 @@ function App() {
 
   return (
     <>
-        <section className="container">
-          
-        </section>
+      <section className="container home">
+        <aside className="img-card">
+          <img src="/img-code.jpg" alt="Imagem de um computador aberto" />
+        </aside>
+        <aside className="home-text-description">
+          <p>Hello there 🖖, eu sou</p>
+          <h2>Juan Severiano</h2>
+          <p>I build accessible, inclusive products and digital experiences for the web.</p>
+        </aside>
+      </section>
     </>
   )
 }
