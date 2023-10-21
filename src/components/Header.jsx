ce2859b411f46e3fb605e194e5f203f778799ab7
Francisco Juan <juansoussev@gmail.com>
2023-10-21 19:58:56 -0300

    add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import { Component } from "react";
+
+export default class Header extends Component {
+    render() {
+        return (
+            <header className="header">
+                <aside className="logo-name">
+                    <h2 className="title">@juansev.dev</h2>
+                </aside>
+                <nav className="navbar">
+                    <ul>
+                        <li><a href="#about-me">about</a></li>
+                        <li><a href="#skills">skills</a></li>
+                        <li><a href="#projects">projects</a></li>
+                        <li><a href="#contact">contact</a></li>
+                    </ul>
+                </nav>
+            </header>
+        )
+    }
+}
