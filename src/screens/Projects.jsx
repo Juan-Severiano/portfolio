7df9a4920519c1033ed34511c1bbb0edbbfe3deb
Francisco Juan <juansoussev@gmail.com>
2023-10-22 00:02:14 -0300

    about concluido

---
+++ src/screens/Projects.jsx
@@ -0,0 +1,18 @@
+import { Component } from "react";
+
+export default class Projects extends Component {
+    render() {
+        return (
+            <section className="container home">
+                <aside className="img-card">
+                  <img src="/img-code.jpg" alt="Imagem de um computador aberto" />
+                </aside>
+                <aside className="home-text-description">
+                  <p>Hello there 🖖, eu sou</p>
+                  <h2>Juan Severiano</h2>
+                  <p className="typewriter">I build accessible, inclusive products and digital experiences for the web.</p>
+                </aside>
+            </section>
+        )
+    }
+}
